f5d2035a9d91c3bba67251adf12c5167950831a7
dshijie <943996999@qq.com>
2023-11-07 14:27:14 +0800

    feat: add user menu icon

--- src/pages/Layout/components/UserDrawer.tsx
+++ src/pages/Layout/components/UserDrawer.tsx
@@ -2,6 +2,7 @@ import { useModel } from '@/.umi/plugin-model';
 import Button from '@/components/Button/Button';
 import { getUserInfoApi } from '@/services/user';
 import { RightOutlined } from '@ant-design/icons';
+import { Icon } from '@umijs/max';
 import { Drawer, List, QRCode } from 'antd';
 import React, { useEffect, useState } from 'react';
 interface Props {
@@ -31,61 +32,205 @@ const UserContent = (props: { userInfo: any }) => {
   const list1: ListItem[] = [
     {
       label: '消息中心',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:message"
+        />
+      ),
     },
     {
       label: '云贝中心',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:clound_bei"
+        />
+      ),
     },
     {
       label: '创作者中心',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:creator"
+        />
+      ),
     },
   ];
   const list2: ListItem[] = [
     {
       label: '趣测',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '云村有票',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '多多西西口袋',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '商城',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: 'Beat专区',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '音乐收藏家',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
   ];
   const list3: ListItem[] = [
     {
       label: '设置',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '深色模式',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '定时关闭',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '个性装扮',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '边听边存',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '在线听歌免流量',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '音乐黑名单',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '青少年模式',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
     {
       label: '音乐闹钟',
+      prefix: (
+        <Icon
+          width="16px"
+          height="16px"
+          className="mr-[4px]"
+          icon="local:setting"
+        />
+      ),
     },
   ];
   const list4: ListItem[] = [
@@ -127,7 +272,7 @@ const UserContent = (props: { userInfo: any }) => {
         </div>
         <RightOutlined />
       </div>
-      <div className="h-[120px] p-[12px] w-full bg-gradient-to-r from-[#393737] to-[#5e5150] mt-[12px] rounded-xl">
+      <div className="h-[116px] p-[12px] w-full bg-gradient-to-r from-[#393737] to-[#5e5150] mt-[12px] rounded-xl">
         <div className="text-[#e0cdc9] font-medium flex justify-between items-center">
           <span className="text-[16px] tracking-wide"> 黑胶VIP·肆</span>
           <span>
@@ -146,25 +291,67 @@ const UserContent = (props: { userInfo: any }) => {
         className="mt-[12px]"
         bordered
         dataSource={list1}
-        renderItem={(item) => <List.Item>{item.label}</List.Item>}
+        renderItem={(item) => (
+          <List.Item>
+            <div className="flex items-center">
+              <div className="flex items-center flex-1">
+                {item.prefix}
+                {item.label}
+              </div>
+              <div>{item.suffix}</div>
+            </div>
+          </List.Item>
+        )}
       ></List>
       <List
         className="mt-[12px]"
         bordered
         dataSource={list2}
-        renderItem={(item) => <List.Item>{item.label}</List.Item>}
+        renderItem={(item) => (
+          <List.Item>
+            {' '}
+            <div className="flex items-center">
+              <div className="flex items-center flex-1">
+                {item.prefix}
+                {item.label}
+              </div>
+              <div>{item.suffix}</div>
+            </div>
+          </List.Item>
+        )}
       ></List>
       <List
         className="mt-[12px]"
         bordered
         dataSource={list3}
-        renderItem={(item) => <List.Item>{item.label}</List.Item>}
+        renderItem={(item) => (
+          <List.Item>
+            {' '}
+            <div className="flex items-center">
+              <div className="flex items-center flex-1">
+                {item.prefix}
+                {item.label}
+              </div>
+              <div>{item.suffix}</div>
+            </div>
+          </List.Item>
+        )}
       ></List>
       <List
         className="mt-[12px]"
         bordered
         dataSource={list4}
-        renderItem={(item) => <List.Item>{item.label}</List.Item>}
+        renderItem={(item) => (
+          <List.Item>
+            <div className="flex items-center">
+              <div className="flex items-center flex-1">
+                {item.prefix}
+                {item.label}
+              </div>
+              <div>{item.suffix}</div>
+            </div>
+          </List.Item>
+        )}
       ></List>
       <List
         className="mt-[12px]"
